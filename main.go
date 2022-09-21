79fe95f6daddd27e9d358b450ad1539dabaa68bc
gab5987 <biell.gb4@gmail.com>
2022-09-21 17:35:19 -0300

    Added measurements class

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+type measurement struct {
+	ID    string  `json:"id"`
+	Value float32 `json:"value"`
+	Date  string  `json:"date"`
+}
+
+var measurements = []measurement{
+	{ID: "1", Value: 1.1, Date: "2019-01-01"},
+}
+
+func main() {
+
+}
