69a4bdba5f37c803b5e32b962fae424d711903aa
gab5987 <biell.gb4@gmail.com>
2022-09-21 17:56:44 -0300

    Feat: Added post route and fixed measurement class

--- main.go
+++ main.go
@@ -3,18 +3,21 @@ package main
 import "github.com/gin-gonic/gin"
 
 type measurement struct {
-	ID    string  `json:"id"`
-	Value float32 `json:"value"`
-	Date  string  `json:"date"`
+	ID          int     `json:"id"`
+	Temperature float32 `json:"temperature"`
+	HeatIndex   float32 `json:"heatIndex"`
+	Humidity    float32 `json:"humidity"`
+	Date        string  `json:"date"`
 }
 
 var measurements = []measurement{
-	{ID: "1", Value: 1.1, Date: "2019-01-01"},
+	{ID: 1, Temperature: 35.7, HeatIndex: 32.5, Humidity: 99.7, Date: "2019-01-01"},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/measurements", getMeasurements)
+	router.POST("/measurements", postMeasurement)
 
 	router.Run("localhost:8080")
 }
--- routes.go
+++ routes.go
@@ -10,3 +10,15 @@ import (
 func getMeasurements(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, measurements)
 }
+
+// postMeasurement adds a new measurement
+func postMeasurement(c *gin.Context) {
+	var newMeasurement measurement
+	if err := c.BindJSON(&newMeasurement); err != nil {
+		return
+	}
+	newMeasurement.ID = measurements[len(measurements)-1].ID + 1
+
+	measurements = append(measurements, newMeasurement)
+	c.IndentedJSON(http.StatusCreated, newMeasurement)
+}
